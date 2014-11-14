63d6ce8ce02f9f8c0658fa57a0e8a4ee9232b37a
djoq <danieljoquinn@gmail.com>
2014-11-13 22:14:37 -0500

    update to restrictions again

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   def self.active
-    where(status: true)
+    @self.active = User.all.where(status: true)
   end
  
 end
