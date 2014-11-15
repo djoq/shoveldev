53a8d55f2cb54b4db43a92d2c9bba1b115a92cfd
djoq <danieljoquinn@gmail.com>
2014-11-15 00:09:22 -0500

    rollback to restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
   respond_to :html, :xml, :json
 
   def index
-    @users = User.Active
+    @users = User.all
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
-      marker.title user.title
+      marker.title user.status
       user_link = view_context.link_to user.title, user_path(user)
       marker.infowindow "<h4><u>#{user_link}</u></h4><i>#{user.address}</i>"
     end
