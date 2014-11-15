495a066e8c2475e546a067aa0331d66d736195e6
djoq <danieljoquinn@gmail.com>
2014-11-15 00:36:00 -0500

    lowercase to Active/Inactive status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   respond_to :html, :xml, :json
 
   def index
-    @users = User.all
+    @users = User.active
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
