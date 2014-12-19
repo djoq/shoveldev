ea1796ceac17c32542e7370123e966886d1b9b89
djoq <danieljoquinn@gmail.com>
2014-12-19 00:40:56 -0500

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.active
+    where(status: true) 
+  end
+
 end
